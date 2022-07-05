2a3a25cd1cad0345bd4c4afbf3564f37081e4f5f
Tadaka Surya Teja <30947856+TadakaSuryaTeja@users.noreply.github.com>
2022-07-05 23:36:05 +0530

    Update p09_string_rotation.py

--- chapter_01/p09_string_rotation.py
+++ chapter_01/p09_string_rotation.py
@@ -2,7 +2,8 @@
 import unittest
 
 
-def string_rotation(s1, s2):
+# O(1)
+def string_rotation(s1: str, s2: str) -> bool:
     if len(s1) == len(s2) != 0:
         return s2 in s1 * 2
     return False
@@ -15,7 +16,8 @@ class Test(unittest.TestCase):
         ("foo", "bar", False),
         ("foo", "foofoo", False),
     ]
-
+    
+    # O(N)
     def test_string_rotation(self):
         for [s1, s2, expected] in self.test_cases:
             actual = string_rotation(s1, s2)
